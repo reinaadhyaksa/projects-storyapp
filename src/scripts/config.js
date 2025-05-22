6184713ff96f31b0699da70e5657f179eb7b8f8e
Reina Adhyaksa <violetevargarde@gmail.com>
2025-05-22 18:06:43 +0700

    woilah

---
+++ src/scripts/config.js
@@ -0,0 +1,5 @@
+export const ACCESS_TOKEN_KEY = 'accessToken_raichan';
+
+export const BASE_URL = 'https://story-api.dicoding.dev/v1/';
+
+export const MAP_SERVICE_API_KEY = 'L1V7oYaAoswTHnKhMMJ8';
---
+++ src/scripts/index.js
@@ -0,0 +1,23 @@
+import '../styles/styles.css';
+import '../styles/responsives.css';
+import 'tiny-slider/dist/tiny-slider.css';
+import 'leaflet/dist/leaflet.css';
+
+import App from './pages/app';
+import Camera from './utils/camera';
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const app = new App({
+    content: document.getElementById('main-content'),
+    drawerButton: document.getElementById('drawer-button'),
+    drawerNavigation: document.getElementById('navigation-drawer'),
+    skipLinkButton: document.getElementById('skip-link'),
+  });
+  await app.renderPage();
+
+  window.addEventListener('hashchange', async () => {
+    await app.renderPage();
+
+    Camera.stopAllStreams();
+  });
+});
